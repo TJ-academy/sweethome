b9b3171adf5969c28031428d41dfe2479258e897
guykim <life4guy@gmail.com>
2025-10-22 10:22:24 +0900

    host function basic, header change

--- src/main/java/com/example/sweethome/user/HostController.java
+++ src/main/java/com/example/sweethome/user/HostController.java
@@ -1,5 +1,50 @@
 package com.example.sweethome.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/host")
+@RequiredArgsConstructor
 public class HostController {
 
+	@GetMapping("/list")
+	public String list(HttpSession session, Model model) {
+
+		User user = (User) session.getAttribute("userProfile");
+		if (user == null)
+			return "redirect:/user/login";
+
+		model.addAttribute("user", user);
+
+		return "host/myHomeList";
+	}
+
+	@GetMapping("/calendar")
+	public String calendar(HttpSession session, Model model) {
+
+		User user = (User) session.getAttribute("userProfile");
+		if (user == null)
+			return "redirect:/user/login";
+
+		model.addAttribute("user", user);
+		return "host/calendar";
+	}
+
+	@GetMapping("/today")
+	public String today(HttpSession session, Model model) {
+
+		User user = (User) session.getAttribute("userProfile");
+		if (user == null)
+			return "redirect:/user/login";
+
+		model.addAttribute("user", user);
+
+		return "host/today";
+	}
 }
